ea2a977e58ad5c47147a36c9ef0b7bb4c3810728
Pashkovskaya Elizaveta <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2022-10-26 10:42:20 +0300

    refactoring

--- javascript/vaihda-kuva/main.js
+++ javascript/vaihda-kuva/main.js
@@ -3,17 +3,16 @@ let text1 = "I choose a dog";
 let text2 = "I choose a cat";
 function changePhoto() {
   let image1 = document.querySelector("img");
+  let button = document.getElementById("btn");
   if (click % 2 == 0) {
-    image1.setAttribute("src", `images/2.jpeg`);
-    document.getElementById("btn").innerText = text2;
-    document.getElementById("btn").style.backgroundColor = "#80A7B8";
-  click++;
-
+    image1.setAttribute("src", 'images/2.jpeg');
+    button.innerText = text2;
+    button.style.backgroundColor = "#80A7B8";
+    click++;
   } else {
-    image1.setAttribute("src", `images/1.jpeg`);
-    document.getElementById("btn").innerText = text1;
-    document.getElementById("btn").style.backgroundColor = "#ECB605 ";
-  click++;
-
+    image1.setAttribute("src",'images/1.jpeg');
+    button.innerText = text1;
+    button.style.backgroundColor = "#ECB605 ";
+    click++;
   }
 }
